f9e24b69c57efe3d39d08d9b3db12d0535acd85e
Kangxun <e0543787@u.nus.edu>
2021-08-18 18:20:40 +0800

    Added Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,6 +22,9 @@ public class Duke {
         // List to store checked items
         boolean[] checkedList = new boolean[100];
 
+        // List to store type of item
+        String[] typeList = new String[100];
+
         // Create a scanner to read from standard input.
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
@@ -29,10 +32,14 @@ public class Duke {
             if (cmd.equals("list")) {
                 System.out.println(line);
                 for (int i = 0; i < count; i++) {
+                    String type = " ";
+                    if (typeList[i] != null) {
+                        type = typeList[i];
+                    }
                     if (checkedList[i]) {
-                        System.out.println((i + 1) + ".[X] " + cmdList[i]);
+                        System.out.println((i + 1) + ".[" + type + "][X] " + cmdList[i]);
                     } else {
-                        System.out.println((i + 1) + ".[ ] " + cmdList[i]);
+                        System.out.println((i + 1) + ".[" + type + "][ ] " + cmdList[i]);
                     }
                 }
                 System.out.println(line);
@@ -42,16 +49,53 @@ public class Duke {
 
                 // Make sure itemNo is within limit
                 if ((itemNo <= count) && (itemNo > 0)) {
+                    String type = " ";
+                    if (typeList[itemNo - 1] != null) {
+                        type = typeList[itemNo - 1];
+                    }
+
                     checkedList[itemNo - 1] = true;
                     System.out.println(line);
                     System.out.println("congrats on completing this task! :");
-                    System.out.println("  [X] " + cmdList[itemNo - 1]);
+                    System.out.println("  [" + type + "][X] " + cmdList[itemNo - 1]);
                 } else {
                     System.out.println(line);
                     cmdList[count] = cmd;
                     count++;
                     System.out.println("added: " + cmd);
                 }
+                System.out.println(line);
+            } else if (cmd.matches("^todo .*$")
+                    || cmd.matches("^deadline .* /by .*$")
+                    || cmd.matches("^event .* /at .*$")) {
+                String[] temp = cmd.split(" ", 2);
+
+                System.out.println(line);
+                System.out.println("noted! ive added this task:");
+
+                // Separate into cases
+                switch (temp[0]) {
+                    case "todo":
+                        typeList[count] = "T";
+                        cmdList[count] = temp[1];
+                        System.out.println("  [" + typeList[count] + "][ ] " + cmdList[count]);
+                        break;
+                    case "deadline":
+                        typeList[count] = "D";
+                        String[] temp_deadline = temp[1].split(" /by ", 2);
+                        cmdList[count] = temp_deadline[0] + " (by: " + temp_deadline[1] +")";
+                        System.out.println("  [" + typeList[count] + "][ ] " + cmdList[count]);
+                        break;
+                    case "event":
+                        typeList[count] = "E";
+                        String[] temp_event = temp[1].split(" /at ", 2);
+                        cmdList[count] = temp_event[0] + " (at: " + temp_event[1] +")";
+                        String type3 = typeList[count];
+                        System.out.println("  [" + typeList[count] + "][ ] " + cmdList[count]);
+                        break;
+                }
+                count++;
+                System.out.println("now you have " + count + " tasks in your list");
                 System.out.println(line);
             } else {
                 System.out.println(line);
