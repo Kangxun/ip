e3e30b2aec60960a9c9cbe00ec17c03ed5103eaa
Kangxun <e0543787@u.nus.edu>
2021-08-18 17:09:46 +0800

    Added Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,21 +19,45 @@ public class Duke {
         String[] cmdList = new String[100];
         int count = 0;
 
+        // List to store checked items
+        boolean[] checkedList = new boolean[100];
+
         // Create a scanner to read from standard input.
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            if (!cmd.equals("list")) {
+            if (cmd.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < count; i++) {
+                    if (checkedList[i]) {
+                        System.out.println((i + 1) + ".[X] " + cmdList[i]);
+                    } else {
+                        System.out.println((i + 1) + ".[ ] " + cmdList[i]);
+                    }
+                }
+                System.out.println(line);
+            } else if (cmd.matches("^done [0-9]+$")){
+                String[] temp = cmd.split(" ");
+                int itemNo = Integer.parseInt(temp[1]);
+
+                // Make sure itemNo is within limit
+                if ((itemNo <= count) && (itemNo > 0)) {
+                    checkedList[itemNo - 1] = true;
+                    System.out.println(line);
+                    System.out.println("congrats on completing this task! :");
+                    System.out.println("  [X] " + cmdList[itemNo - 1]);
+                } else {
+                    System.out.println(line);
+                    cmdList[count] = cmd;
+                    count++;
+                    System.out.println("added: " + cmd);
+                }
+                System.out.println(line);
+            } else {
                 System.out.println(line);
                 cmdList[count] = cmd;
                 count++;
                 System.out.println("added: " + cmd);
-                System.out.println(line);
-            } else {
-                System.out.println(line);
-                for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + cmdList[i]);
-                }
                 System.out.println(line);
             }
             cmd = sc.nextLine();
