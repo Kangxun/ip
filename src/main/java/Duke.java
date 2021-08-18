5bc220b8fffd0c7c8ddf8f6a669636388540aa71
Kangxun <e0543787@u.nus.edu>
2021-08-18 15:42:04 +0800

    Fixed Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,7 +18,7 @@ public class Duke {
         // Create a scanner to read from standard input.
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
-        while (cmd.equals("bye")) {
+        while (!cmd.equals("bye")) {
             System.out.println(line);
             System.out.println(cmd);
             System.out.println(line);
